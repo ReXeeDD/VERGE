736e962f32d2e8d22c66cf3cbaa976cc275bf9d0
Albin Thomas <albinthomas7034@gmail.com>
2026-02-27 02:24:45 +0530

    added more function to vec2 class

--- Verge/src/Verge/Physics/Vec2.h
+++ Verge/src/Verge/Physics/Vec2.h
@@ -13,6 +13,41 @@ struct Vec2 {
 	Vec2 operator-(const Vec2& other)const {
 		return { x - other.x,y - other.y };
 	}
+	Vec2 operator*(const float& scalar) {
+		return { x * scalar, y * scalar };
+	}
+    Vec2 operator/(float scalar) const
+	{
+		return { x / scalar, y / scalar };
+	}
+    Vec2& operator+=(const Vec2& other)
+    {
+        x += other.x;
+        y += other.y;
+        return *this;
+    }
+
+    Vec2& operator-=(const Vec2& other)
+    {
+        x -= other.x;
+        y -= other.y;
+        return *this;
+    }
+
+    Vec2& operator*=(float scalar)
+    {
+        x *= scalar;
+        y *= scalar;
+        return *this;
+    }
+
+    Vec2& operator/=(float scalar)
+    {
+        x /= scalar;
+        y /= scalar;
+        return *this;
+    }
+
 };
 
 inline std::ostream& operator<<(std::ostream& stream, const Vec2& v) {
